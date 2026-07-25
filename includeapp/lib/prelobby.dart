302a14c07756145c095201683d5b6c03c5df96c4
Thiago Ramos <07thiagoramos@gmail.com>
2026-07-25 13:28:27 -0300

    Responsivando por meio do if

--- includeapp/lib/prelobby.dart
+++ includeapp/lib/prelobby.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:just_bubble/just_bubble.dart';
 import 'inicial.dart';
@@ -30,11 +29,43 @@ TextEditingController nomeUs = TextEditingController();
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text("Prelobby")),
-      body: SafeArea(
+      body: Center(
         child: LayoutBuilder(
           builder: (context, constraints) {
             final telaWidth = constraints.maxWidth;
             final telaHeight = constraints.maxHeight;
+            final telaPC = telaWidth > 600;
+            double pessoaWidth;
+            double pessoaEsquerdo;
+            double pessoaTop;
+            double fontSizeBalao;
+            double fontSizeElevated;
+            double botaoPadding;
+            double balaoTop;
+            double balaoEsquerda;
+            double balaoDireito;
+
+            if (telaPC) {
+              pessoaWidth = telaWidth * 0.2;
+              pessoaEsquerdo = telaWidth * 0.30;
+              pessoaTop = telaHeight * 0.05;
+              fontSizeBalao = telaWidth * 0.015;
+              fontSizeElevated = telaWidth * 0.02;
+              botaoPadding = telaWidth * 0.0075;
+              balaoTop = telaHeight * 0.18;
+              balaoEsquerda = telaWidth * 0.46;
+              balaoDireito = telaWidth * 0.32;
+            } else {
+              pessoaWidth = telaWidth * 0.40;
+              pessoaEsquerdo = telaWidth * 0.10;
+              pessoaTop = telaHeight * 0.1;
+              fontSizeBalao = telaWidth * 0.045;
+              fontSizeElevated = telaWidth * 0.07;
+              botaoPadding = telaHeight * 0.015;
+              balaoTop = telaHeight * 0.22;
+              balaoEsquerda = telaWidth * 0.45;
+              balaoDireito = telaWidth * 0.16;
+            }
 
             return Column(
               children: [
@@ -42,19 +73,19 @@ TextEditingController nomeUs = TextEditingController();
                   child: Stack(
                     children: [
                       Positioned(
-                        left: telaWidth * 0.12,
+                        left: pessoaEsquerdo,
                         bottom: 0,
-                        top: telaHeight * 0.1,
+                        top: pessoaTop,
                         child: Image.asset(
                           "assets/imagem/MeninoIncludeAPP.png",
-                          width: telaWidth * 0.40,
+                          width: pessoaWidth,
                         ),
                       ),
 
                       Positioned(
-                        left: telaWidth * 0.45,
-                        top: telaHeight * 0.22,
-                        right: telaWidth * 0.16,
+                        left: balaoEsquerda,
+                        top: balaoTop,
+                        right: balaoDireito,
                         child: Bubble(
                           padding: EdgeInsets.all(12),
                           color: Colors.white,
@@ -68,7 +99,7 @@ TextEditingController nomeUs = TextEditingController();
                           ),
                           child: Text(
                             "Olá, seja muito bem-vindo ao IncludeAPP", 
-                            style: TextStyle(fontSize: telaWidth * 0.045), 
+                            style: TextStyle(fontSize: fontSizeBalao), 
                           ),
                         ),
                       )
@@ -76,7 +107,7 @@ TextEditingController nomeUs = TextEditingController();
                   ), 
                 ),
                 Padding(
-                  padding: EdgeInsetsGeometry.only(bottom: telaHeight * 0.015),
+                  padding: EdgeInsetsGeometry.only(bottom: botaoPadding),
                   child: SizedBox(
                     width: telaWidth * 0.8,
                     height: telaHeight * 0.065,
@@ -86,7 +117,7 @@ TextEditingController nomeUs = TextEditingController();
                         foregroundColor: Colors.white
                       ),
                       onPressed: () {
-                    }, child: Text("Continuar", style: TextStyle(fontSize: telaWidth * 0.07),)),
+                    }, child: Text("Continuar", style: TextStyle(fontSize: fontSizeElevated),)),
                   ),
                 )
               ],
