ac046fd0c5d68114a889d6883631548ffcd3a98a
Thiago Ramos <07thiagoramos@gmail.com>
2026-08-03 23:00:34 -0300

    Botão de configurações no canto superior direito e no drawer

--- includeapp/lib/ funcoes_widgets/widgets.dart
+++ includeapp/lib/ funcoes_widgets/widgets.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:includeapp/%20funcoes_widgets/configura%C3%A7%C3%B5es.dart';
 import 'package:includeapp/%20funcoes_widgets/funcoes.dart';
+import 'package:includeapp/inicial.dart';
+import "package:includeapp/nivelUm.dart";
 
-Widget drawerFazer(BuildContext context) {
+Widget drawerFazer(BuildContext context, String nome) {
   return Drawer(
     child: ListView(
       children: [
@@ -23,6 +26,9 @@ Widget drawerFazer(BuildContext context) {
               fontSize: 20
             ),
           ),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Inicial(nome: nome)));
+          },
         ),
 
         ListTile(
@@ -50,9 +56,45 @@ Widget drawerFazer(BuildContext context) {
               fontSize: 20
             ),
           ),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Configuracoes(nome: nome)));
+          },
         ),
       ],
     ),
+  );
+}
+
+// ===
+
+PreferredSizeWidget appBarWidget(BuildContext context) {
+  return AppBar(
+    centerTitle: true, title: Text("Tela Inicial", style: TextStyle(color: Colors.white),),
+    iconTheme: IconThemeData(
+      color: Colors.white
+    ),
+    actions: [
+      Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(20)
+        ),
+        child: Row(
+          children: [
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF2563EB),
+              ),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => NivelUm()));
+              },
+              icon: Icon(Icons.person_2, color: Colors.white, size: 20,), 
+              label: Text("Level: 0", style: TextStyle(color: Colors.white, fontSize: 16),)),
+          ],              
+        )    
+      ),
+      SizedBox(width: 10),
+    ],
   );
 }
 
