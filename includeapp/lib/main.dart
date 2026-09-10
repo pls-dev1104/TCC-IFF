c92202d714e8e65cbcfc6d4203b73a6f3dc27c8d
pls_dev1104 <plswork1104@gmail.com>
2026-09-09 21:39:41 -0300

    Puxando antes da merda

--- includeapp/lib/main.dart
+++ includeapp/lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:includeapp/banco_de_dados/firebase_options.dart';
 import 'package:includeapp/home.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const new({super.key});
 
   @override
   Widget build(BuildContext context) {
